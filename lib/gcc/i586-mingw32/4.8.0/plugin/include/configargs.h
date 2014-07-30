80f6e7a7983aa5472af6d6a33310233f0c88690e
Romain Beauxis <toots@rastageeks.org>
2014-07-30 11:22:28 -0500

    Initial commit..

---
+++ lib/gcc/i586-mingw32/4.8.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.8.0/configure --target=i586-mingw32 --build=i686-apple-darwin10 --prefix=/usr/local/gcc-4.8.0-qt-4.8.4-for-mingw32/win32-gcc --disable-multilib --enable-threads --enable-sjlj-exceptions --enable-version-specific-runtime-libs --enable-languages=c,c++ --disable-shared CC='cc -O2'";
+static const char thread_model[] = "win32";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "pentium" }, { "arch", "pentium" } };
